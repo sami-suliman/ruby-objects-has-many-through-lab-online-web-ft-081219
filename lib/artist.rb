3745b5cdf9e930d53020f4f444dd1a9145c73a4b
sami suliman <sami.bco@gmail.com>
2019-09-02 16:01:21 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,26 +1,22 @@
-class Artist 
-  attr_accessor :name, :songs 
-  
-  @@all = []
-  
-  require 'pry'
+class Artist
+  attr_accessor :name, :songs
+
   def initialize(name)
-    @name = name 
-    @songs = []
-    @@all << self
-  end 
-  
-  def self.all 
-    @@all
-  end 
-  
-  def songs 
-    @songs
-  end 
-  
-  def genre 
-    self.songs.collect do |song|
-      song.genre 
-    end 
-  end 
-end 
+      @name = name
+      @songs = []
+  end
+
+  def add_song(song)
+      @songs << song
+      song.artist = self
+  end
+
+  def songs
+      @songs
+  end
+
+  def genres
+    self.songs.collect do |song| 
+      song.genre
+    end
+  end
