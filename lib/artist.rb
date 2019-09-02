259e6c7d81130735448796c02d649de0aa2bcb82
sami suliman <sami.bco@gmail.com>
2019-09-02 15:40:18 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,20 +1,10 @@
 class Artist 
-  attr_accessor :name     
+  attr_accessor :name
   
   @@all = []
   
   def initialize(name)
-    @name = name
-   
-    @@all << self 
+    @name = name 
+    @@all << self
   end 
-  
-  def self.all 
-    @@all
-  end 
-  
-  
-end
-  
-  
 end 
